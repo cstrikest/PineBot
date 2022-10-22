ad684fed6a2ee918a35dbda7bdb7c575e8e2ad7a
Pineapple <cstrikest@gmail.com>
2022-10-22 14:16:41 +0900

    Create project.

---
+++ bot.py
@@ -0,0 +1,9 @@
+﻿# Press the green button in the gutter to run the script.
+
+import nonebot as n
+from os import path
+
+if __name__ == '__main__':
+    n.init()
+    n.load_plugins("./Pinebot_main/plugins", "Pinebot_main.plugins")
+    n.run(host = "127.0.0.1", port = 16241, debug = False)
