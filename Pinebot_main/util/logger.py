ea69aee1450ad15b7dfdea920f21d97a7f640f63
Pineapple <cstrikest@gmail.com>
2022-10-24 16:25:12 +0900

    v1.0.1.5 fixed bug. write log部分bug

--- Pinebot_main/util/logger.py
+++ Pinebot_main/util/logger.py
@@ -9,4 +9,5 @@ MSG_LOG_FILE_PATH = "./msg_log.log"
 
 def add_log(ctx, msg: str):
 	with open(MSG_LOG_FILE_PATH, "a", encoding = "utf-8") as f:
-		f.write("[{}] QQ:{} Group: {} Group_card:{} Nickname:{} || {}".format(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), ctx["sender"]["user_id"],ctx["group_id"] , ctx["sender"]["card"], ctx["sender"]["nickname"], msg))
+		f.write("[{}] QQ:{} Group: {} Group_card:{} Nickname:{} || {}\n".format(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), ctx["sender"]["user_id"],ctx["group_id"] , ctx["sender"]["card"], ctx["sender"]["nickname"], msg))
+		f.flush()
