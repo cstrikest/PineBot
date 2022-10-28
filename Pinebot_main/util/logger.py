a4a209c69bbb77ab9905f7d28ae5478fd02ff31d
Pineapple <cstrikest@gmail.com>
2022-10-28 09:14:52 +0900

    v1.1.4.2 更新logger。

--- Pinebot_main/util/logger.py
+++ Pinebot_main/util/logger.py
@@ -11,11 +11,10 @@ def add_log(event_name: str, ctx, msg: str):
 	with open(MSG_LOG_FILE_PATH, "a", encoding = "utf-8") as f:
 		f.write("[{} - {}] QQ:{} Group: {} Group_card:{} Nickname:{} || {}\n".format(
 			time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), event_name, ctx["sender"]["user_id"], ctx["group_id"],
-			ctx["sender"]["card"], ctx["sender"]["nickname"], msg))
+			ctx["sender"]["card"], ctx["sender"]["nickname"], msg).replace("\n", "") + "\n")
 		f.flush()
 
-def add_raw_log(type:str, msg: str):
+def add_raw_log(event_name: str, msg: str):
 	with open(MSG_LOG_FILE_PATH, "a", encoding = "utf-8") as f:
-		f.write("[{} - {}] {}".format(type, time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), msg))
+		f.write("[{} - {}] {}".format(time.strftime("%Y-%m-%d %H:%M:%S",time.localtime()), event_name, msg).replace("\n", "") + "\n")
 		f.flush()
-	
